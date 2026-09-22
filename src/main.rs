c2199a3298b7dda290e859fe25ec757b1bbf4a25
Dmitriy Barabanov <qbarabanov@gmail.com>
2026-09-22 20:12:14 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::println;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Error!");
+
+    let path = Path::new(input.trim());
+    let file = fs::File::open(path);
+    let result = file.is_ok();
+    if result {
+        let bytes = fs::read(path);
+        match bytes {
+            Ok(_) => println!("success"),
+            Err(_) => println!("failure"),
+        }
+    } else {
+        println!("failure");
+    }
 }
